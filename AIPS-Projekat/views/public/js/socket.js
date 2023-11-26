a2b29c3b2e3c21bc50d1b3f9bcd9fb87c260bf2a
Vidosava Arsic <arsic.vida@gmail.com>
2023-11-26 23:58:58 +0100

    Added game logic- king part ||
    King can move left, right, left and right.

--- AIPS-Projekat/views/public/js/socket.js
+++ AIPS-Projekat/views/public/js/socket.js
@@ -540,5 +540,82 @@ socket.on('proslediPomeriFiguru',(options) =>{
     colorInRed(listCrveno);
   }
   function dozvoljeniPoteziKing(xx, yy, elementiSlikeFigure){
+
+    let listZeleno = [];
+    let listCrveno = [];
+    let newX = null;
+    let newY = null;
+    const mojField = document.querySelector('[data-x="'+ xx +'"][data-y="' + yy + '"]');
+    const mojaKlasaFigure = mojField.children[0].getAttribute("class").split("_");
+    //down
+       newX = xx;
+       newY = yy + 1;
+      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else{
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+            }
+          }
+      }
+    //up
+       newX = xx;
+       newY = yy - 1;
+      field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+            listZeleno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else{
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+            }
+            }
+      }
+    //right
+      newX = xx + 1;
+      newY = yy;
+      field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+     if(field){
+       if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+        }else{
+          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+          {
+          listCrveno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }
+        }
+     }
+    //left
+      newX = xx - 1;
+      newY = yy;
+     field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+     if(field){
+       if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+        }else{
+          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+          {
+          listCrveno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }
+        }
+     }
+    colorInGreen(listZeleno);
+    colorInRed(listCrveno);
     dozvoljeniPoteziBishop(xx, yy, 2);
   }
