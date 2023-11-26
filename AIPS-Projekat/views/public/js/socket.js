35b842c67813c06e07e0be7b294fdd7d16bc0c5b
Vidosava Arsic <arsic.vida@gmail.com>
2023-11-27 00:53:00 +0100

    Logic update
    code refactoring for knight

--- AIPS-Projekat/views/public/js/socket.js
+++ AIPS-Projekat/views/public/js/socket.js
@@ -629,142 +629,48 @@ socket.on('proslediPomeriFiguru',(options) =>{
     let newY = null;
     const mojField = document.querySelector('[data-x="'+ xx +'"][data-y="' + yy + '"]');
     const mojaKlasaFigure = mojField.children[0].getAttribute("class").split("_");
-    //down-left-left
+    let listXY = [];
        newX = xx - 2;
        newY = yy + 1;
-      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-      if(field){
-        if(!field.children[0]){
-          listZeleno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }else{
-            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-            {
-            listCrveno.push({x:newX,y:newY});
-            listaDozvoljenihPoteza.push({x:newX,y:newY});
-            }
-          }
-      }
-    //down-left
+       listXY.push({x:newX, y:newY});
        newX = xx - 1;
        newY = yy + 2;
-      field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-      if(field){
-        if(!field.children[0]){
-            listZeleno.push({x:newX,y:newY});
-            listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }else{
-            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-            {
-            listCrveno.push({x:newX,y:newY});
-            listaDozvoljenihPoteza.push({x:newX,y:newY});
+       listXY.push({x:newX, y:newY});
+       newX = xx + 2;
+       newY = yy + 1;
+       listXY.push({x:newX, y:newY});
+       newX = xx + 1;
+       newY = yy + 2;
+       listXY.push({x:newX, y:newY});
+       newX = xx - 2;
+       newY = yy - 1;
+       listXY.push({x:newX, y:newY});
+       newX = xx - 1;
+       newY = yy - 2;
+       listXY.push({x:newX, y:newY});
+       newX = xx + 2;
+       newY = yy - 1;
+       listXY.push({x:newX, y:newY});
+       newX = xx + 1;
+       newY = yy - 2;
+       listXY.push({x:newX, y:newY});
+       listXY.forEach((element) => {
+        let field = document.querySelector('[data-x="'+ element.x +'"][data-y="' + element.y + '"]');
+        if(field){
+          if(!field.children[0]){
+            listZeleno.push({x:element.x,y:element.y});
+            listaDozvoljenihPoteza.push({x:element.x,y:element.y});
+            }else{
+              const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+              if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+              {
+              listCrveno.push({x:element.x,y:element.y});
+              listaDozvoljenihPoteza.push({x:element.x,y:element.y});
+              }
             }
-            }
-      }
-    //down-right-right
-      newX = xx + 2;
-      newY = yy + 1;
-      field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-     if(field){
-       if(!field.children[0]){
-          listZeleno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }else{
-          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-          {
-          listCrveno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }
         }
-     }
-    //down-right
-      newX = xx + 1;
-      newY = yy + 2;
-     field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-     if(field){
-       if(!field.children[0]){
-          listZeleno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }else{
-          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-          {
-          listCrveno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }
-        }
-     }
-  //up-left-left
-     newX = xx - 2;
-     newY = yy - 1;
-    field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-    if(field){
-      if(!field.children[0]){
-        listZeleno.push({x:newX,y:newY});
-        listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }else{
-          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-          {
-          listCrveno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }
-        }
-    }
-  //up-left
-     newX = xx - 1;
-     newY = yy - 2;
-    field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-    if(field){
-      if(!field.children[0]){
-          listZeleno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }else{
-          const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-          if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-          {
-          listCrveno.push({x:newX,y:newY});
-          listaDozvoljenihPoteza.push({x:newX,y:newY});
-          }
-          }
-    }
-  //up-right-right
-    newX = xx + 2;
-    newY = yy - 1;
-    field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-   if(field){
-     if(!field.children[0]){
-        listZeleno.push({x:newX,y:newY});
-        listaDozvoljenihPoteza.push({x:newX,y:newY});
-      }else{
-        const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-        if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-        {
-        listCrveno.push({x:newX,y:newY});
-        listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }
-      }
-   }
-  //up-right
-    newX = xx + 1;
-    newY = yy - 2;
-   field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
-   if(field){
-     if(!field.children[0]){
-        listZeleno.push({x:newX,y:newY});
-        listaDozvoljenihPoteza.push({x:newX,y:newY});
-      }else{
-        const klasaFigure =  field.children[0].getAttribute("class").split("_");         
-        if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
-        {
-        listCrveno.push({x:newX,y:newY});
-        listaDozvoljenihPoteza.push({x:newX,y:newY});
-        }
-      }
-   }
+       });
+      
     colorInGreen(listZeleno);
     colorInRed(listCrveno);
   }
