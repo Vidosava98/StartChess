4efd18aeff6922d7253e7ea4bbbcefca62bfdc40
Vidosava Arsic <arsic.vida@gmail.com>
2023-11-26 22:29:05 +0100

    Added game logic rook
    Added game logic rook, available movement , green and red field.

--- AIPS-Projekat/views/public/js/socket.js
+++ AIPS-Projekat/views/public/js/socket.js
@@ -134,6 +134,9 @@ socket.on('proslediPomeriFiguru',(options) =>{
       case "pawn":
         dozvoljeniPoteziPawn(xx, yy, elementiSlikeFigure);
         break;
+      case "rook":
+        dozvoljeniPoteziRook(xx, yy, elementiSlikeFigure);
+        break;
       default:
         listaDozvoljenihPoteza = [];
     }
@@ -330,4 +333,100 @@ socket.on('proslediPomeriFiguru',(options) =>{
     else
     { el.style.backgroundColor = "#FFF"; }
     });
+  }
+  function dozvoljeniPoteziRook(xx, yy, elementiSlikeFigure){
+    let listZeleno = [];  
+    let listCrveno = [];
+    const mojField = document.querySelector('[data-x="'+ xx +'"][data-y="' + yy + '"]');
+    const mojaKlasaFigure = mojField.children[0].getAttribute("class").split("_");
+    let newX = null;
+    let newY = null;
+
+    for(let i = 1; i < 8; i++){
+      //left
+      newX = xx - i;
+      newY = yy;
+      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else
+          {
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+            }
+            break;
+          }
+      }
+    }
+    for(let i = 1; i < 8; i++){
+      //right
+      newX = xx + i;
+      newY = yy;
+      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else
+          {
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+            }
+            break;
+          }
+      }
+    }
+    for(let i = 1; i < 8; i++){
+      //up
+      newX = xx;
+      newY = yy - i;
+      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else
+          {
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});
+            }
+            break;
+          }
+      }
+    }
+    for(let i = 1; i < 8; i++){
+      //down
+      newX = xx;
+      newY = yy + i;
+      let field = document.querySelector('[data-x="'+ newX +'"][data-y="' + newY + '"]');
+      if(field){
+        if(!field.children[0]){
+          listZeleno.push({x:newX,y:newY});
+          listaDozvoljenihPoteza.push({x:newX,y:newY});
+          }else
+          {
+            const klasaFigure =  field.children[0].getAttribute("class").split("_");         
+            if(klasaFigure[0].toString() !== mojaKlasaFigure[0].toString())
+            {
+            listCrveno.push({x:newX,y:newY});
+            listaDozvoljenihPoteza.push({x:newX,y:newY});      
+            }
+            break;
+          }
+      }
+    }
+
+    colorInGreen(listZeleno);
+    colorInRed(listCrveno);
   }
